357b6a87cacd7bdab933dff90c6f8c6043cf2adc
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-25 17:47:19 -0300

    chore: implementing form and button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,14 +5,28 @@ import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { ChevronDown, LucideAngularModule } from 'lucide-angular';
 import { EntryComponent } from './components/entry/entry.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BaseFormComponent } from './components/base-form/base-form.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [CapitalizePipe, DropdownComponent, EntryComponent],
+  declarations: [
+    CapitalizePipe,
+    DropdownComponent,
+    EntryComponent,
+    BaseFormComponent,
+    ButtonComponent,
+  ],
   imports: [
     CommonModule,
     LucideAngularModule.pick({ ChevronDown }),
     ReactiveFormsModule,
   ],
-  exports: [CapitalizePipe, DropdownComponent, EntryComponent],
+  exports: [
+    CapitalizePipe,
+    DropdownComponent,
+    EntryComponent,
+    BaseFormComponent,
+    ButtonComponent,
+  ],
 })
 export class SharedModule {}
