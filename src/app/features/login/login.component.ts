b566f90c2a73dd7af94cbec0646991db2ec3ddd5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-25 18:49:25 -0300

    feat: login page

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, signal, WritableSignal } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,12 +9,45 @@ import { Component, signal, WritableSignal } from '@angular/core';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  // passwordType: 'text' | 'password' = 'password';
   passwordType: WritableSignal<'text' | 'password'> = signal('password');
   i = 0;
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  constructor(
+    private readonly toastr: ToastrService,
+    private readonly auth: AuthService
+  ) {}
+
   submit() {
-    console.log('hi');
+    if (this.loginForm.invalid) {
+      if (this.loginForm.get('email')?.hasError('email')) {
+        this.toastr.error('Insira um email válido.', 'Erro!');
+        return;
+      }
+
+      if (this.loginForm.get('password')?.hasError('minlength')) {
+        this.toastr.error(
+          'A senha precisa ter no minimo 8 caracteres.',
+          'Erro!'
+        );
+        return;
+      }
+
+      this.toastr.error('Todos os dados são obrigatórios.', 'Atenção');
+      return;
+    }
+
+    const email = this.loginForm.get('email')?.value ?? '';
+    const password = this.loginForm.get('password')?.value ?? '';
+
+    this.auth.login(email, password);
   }
 
   changePasswordType() {
