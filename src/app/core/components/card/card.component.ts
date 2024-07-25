1a60fd901411892ce9cfb8ea3953dacfe6f01cc8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-25 16:56:40 -0300

    chore: card component

---
+++ src/app/core/components/card/card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, CardComponent],
   imports: [CommonModule, SharedModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, CardComponent],
 })
 export class CoreModule {}
