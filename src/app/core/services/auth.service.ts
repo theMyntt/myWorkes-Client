4e4e2ee0423889f7ab0c8f2e6525c68b6fe4ce64
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-25 16:35:41 -0300

    feat: logout method

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private readonly cookieService: CookieService) {}
+  constructor(
+    private readonly cookieService: CookieService,
+    private readonly router: Router
+  ) {}
+
+  logout() {
+    this.cookieService.deleteAll();
+    this.router.navigate(['/login']);
+  }
 
   getUser() {
     return {
