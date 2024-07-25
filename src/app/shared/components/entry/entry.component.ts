468a1d8ea47864452d23e53b03edb574c5c011ec
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-25 16:49:05 -0300

    feat: entry component

---
+++ src/app/shared/components/entry/entry.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { TokenUtil } from '../../utils/token.util';
+import { FormControl } from '@angular/forms';
+
+type TEntry = 'text' | 'password';
+
+@Component({
+  selector: 'entry',
+  templateUrl: './entry.component.html',
+  styleUrl: './entry.component.scss',
+})
+export class EntryComponent {
+  @Input({ required: true })
+  type: TEntry = 'text';
+
+  @Input()
+  label: string = '';
+
+  @Input()
+  placeholder: string = '';
+
+  @Input()
+  control!: FormControl;
+
+  id: string;
+
+  constructor(private readonly token: TokenUtil) {
+    this.id = token.generate();
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { ChevronDown, LucideAngularModule } from 'lucide-angular';
+import { EntryComponent } from './components/entry/entry.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [CapitalizePipe, DropdownComponent],
-  imports: [CommonModule, LucideAngularModule.pick({ ChevronDown })],
-  exports: [CapitalizePipe, DropdownComponent],
+  declarations: [CapitalizePipe, DropdownComponent, EntryComponent],
+  imports: [
+    CommonModule,
+    LucideAngularModule.pick({ ChevronDown }),
+    ReactiveFormsModule,
+  ],
+  exports: [CapitalizePipe, DropdownComponent, EntryComponent],
 })
 export class SharedModule {}
