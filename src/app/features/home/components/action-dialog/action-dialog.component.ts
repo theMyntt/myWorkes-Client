8301665578db9fe8c897f119d4255c646be27776
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-29 20:43:21 -0300

    feat: action-dialog component

---
+++ src/app/features/home/components/action-dialog/action-dialog.component.ts
@@ -0,0 +1,58 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IHomeResponse } from '../../home.component';
+import { EEmployeer } from '../../../../types/employeer.type';
+
+@Component({
+  selector: 'action-dialog',
+  templateUrl: './action-dialog.component.html',
+  styleUrls: ['./action-dialog.component.scss'],
+})
+export class ActionDialogComponent implements OnChanges {
+  @Input({ required: true })
+  info!: IHomeResponse;
+
+  @Output()
+  closeDialog = new EventEmitter<IHomeResponse>();
+
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    type: new FormControl<EEmployeer>(0, [Validators.required]),
+    allowedSystems: new FormControl(<string[]>[], [Validators.required]),
+  });
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['info'] && this.info) {
+      this.userForm.patchValue({
+        allowedSystems: this.info.allowedSystems,
+        email: this.info.email,
+        name: this.info.name,
+        phone: this.info.phone,
+        type: this.info.type,
+      });
+    }
+  }
+
+  close(): void {
+    this.closeDialog.emit({
+      allowedSystems: <string[]>[],
+      createdAt: new Date(),
+      email: '',
+      id: '',
+      name: '',
+      password: '',
+      phone: '',
+      type: EEmployeer.employeer,
+      updatedAt: new Date(),
+    });
+  }
+}
